0b1ff0d5565caab36ab660a3c90bffd62882888e
Attila Farago <attila.farago.hu@gmail.com>
2023-01-27 14:00:02 +0100

    * removed python client - no time to finish and test in quality

--- example_clients/_openapi_generate_py.cmd
+++
@@ -1,5 +0,0 @@
-@cls
-@echo ================
-
-@echo Generating client lib
-cmd /c openapi-generator-cli generate -i http://localhost:8080/swagger.json -g python-legacy -o python --global-property=apiTests=false,modelTests=false,modelDocs=false,apiDocs=false --additional-properties=usePromises=true
